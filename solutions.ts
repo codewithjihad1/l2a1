cf25a75c6f74ea8b8b0ef09bc2c86c55c176f51b
unknown <mdjihadhossain793@gmail.com>
2026-05-07 20:34:00 +0600

    All problems solved

--- solutions.ts
+++ solutions.ts
@@ -1,17 +1,16 @@
+// Problem 1
 function filterEvenNumbers(numbers: number[]): number[] {
     const results: number[] = numbers.filter((num) => num % 2 === 0);
     return results;
 }
 
-// console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]));
-
+// Problem 2
 function reverseString(str: string): string {
     const reversed: string = str.split("").reverse().join("");
     return reversed;
 }
 
-// console.log(reverseString("typescript"));
-
+// Problem 3
 function checkType(str: string | number): string {
     let result: string;
 
@@ -23,5 +22,54 @@ function checkType(str: string | number): string {
     return result;
 }
 
-// console.log(checkType("Hello"));
-// console.log(checkType(42));
+// Problem 4
+function getProperty<ObjectType, KeyType extends keyof ObjectType>(
+    obj: ObjectType,
+    key: KeyType,
+): ObjectType[KeyType] {
+    return obj[key];
+}
+
+// Problem 5
+interface Book {
+    title: string;
+    author: string;
+    publishedYear: number;
+}
+
+function toggleReadStatus(book: Book): Book & { isRead: boolean } {
+    return {
+        ...book,
+        isRead: true
+    };
+}
+
+
+// Problem 6
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Student extends Person {
+    grade: string;
+
+    constructor(name: string, age: number, grade: string) {
+        super(name, age);
+        this.grade = grade;
+    }
+
+    getDetails(): string {
+        return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+    }
+}
+
+// Problem 7
+function getIntersection(array1: number[], array2: number[]): number[] {
+    return array1.filter((number) => array2.includes(number));
+}
